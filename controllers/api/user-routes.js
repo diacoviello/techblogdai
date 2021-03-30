4495a420b9e92f9b1a144209851746d2e86e7d2c
Dave <diacovmusic@gmail.com>
2021-03-30 13:58:23 -0400

    added into new code all my changes from previous attempt

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
+  console.log("POST /user");
   try {
     const newUser = await User.create({
       // TODO: SET USERNAME TO USERNAME SENT IN REQUEST
